e5db0f627792fb0ecfbbde721e3c5f786c3659b7
ErwinSturluson <angelmercuries@gmail.com>
2021-03-27 15:05:14 +0300

    Added 'TestFetch.cs' file.

---
+++ TestFetch/TestFetch.cs
@@ -0,0 +1,12 @@
+namespace Test
+{
+    internal class Fetch
+    {
+        internal void Execute()
+        {
+            string message = "Success fetch.";
+
+            Console.WriteLine(message);
+        }
+    }
+}
